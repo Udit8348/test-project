75cd90a890516a2345fb87646db1b8804dcd90c7
Udit S <udit610@yahoo.com>
2020-11-04 19:26:02 -0500

    resolve broken includes

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -18,7 +18,7 @@
   */
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
-#include "main.h"
+#include "../Inc/main.h"
 #include "usart.h"
 #include "gpio.h"
 
